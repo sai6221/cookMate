85a9675f00d5ce822bf7769fbbc05fff71819979
CAJETAN RODRIGUES <cajetan.rodrigues@sjsu.edu>
2023-03-09 18:52:07 -0800

    Add View Binding

--- app/src/main/java/edu/sjsu/android/cookmate/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/cookmate/MainActivity.java
@@ -3,12 +3,16 @@ package edu.sjsu.android.cookmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import edu.sjsu.android.cookmate.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
